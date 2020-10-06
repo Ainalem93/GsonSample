de9fe18354daa29d7c58f45e7e2eea47c3fa1a93
Ainalem93 <melaniapacheco93@gmail.com>
2020-10-06 20:08:39 +0100

    proyecto Gson

---
+++ src/main/java/dad/GsonSample/Datos.java
@@ -0,0 +1,28 @@
+package dad.GsonSample;
+
+public class Datos {
+
+	String Nombre;
+	String Apellidos;
+	Integer Edad;
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellidos() {
+		return Apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+	public Integer getEdad() {
+		return Edad;
+	}
+	public void setEdad(Integer edad) {
+		Edad = edad;
+	}
+	
+	
+}
---
+++ src/main/java/dad/GsonSample/Persona.java
@@ -0,0 +1,37 @@
+package dad.GsonSample;
+
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Persona {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Datos p = new Datos();
+
+		System.out.println("Introduce los datos de las personas: ");
+		p.Nombre = scanner.nextLine();
+		p.Apellidos = scanner.nextLine();
+		p.Edad = scanner.nextInt();
+
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(p);
+		System.out.println(json);
+
+		/*
+		 * Ejemplo de salida en formato JSON generada por la aplicación:
+		 * 
+		 * { 
+		 * "nombre": "Chuck", 
+		 * "apellidos": "Norris", 
+		 * "edad": 67 
+		 * }
+		 */
+
+scanner.close();
+	}
+
+}
